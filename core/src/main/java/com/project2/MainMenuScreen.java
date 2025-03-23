79fd8daf9cb458d67b3cee03616503401f5c402d
Joshua <its7me@gmail.com>
2025-03-23 15:23:42 -0700

    added rocket to main menu and centered it

--- core/src/main/java/com/project2/MainMenuScreen.java
+++ core/src/main/java/com/project2/MainMenuScreen.java
@@ -15,70 +15,72 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 
 public class MainMenuScreen implements Screen {
 
-    private Game game; // Store reference to game
+    private Game game;
     private Stage stage;
-    private SpriteBatch batch;
     private Texture backgroundTexture;
     private Texture titleTexture;
     private Texture startButtonTexture;
     private Texture leaderboardButtonTexture;
+    private Texture rocketTexture;
 
     public MainMenuScreen(Game game) {
-        this.game = game; // Initialize game reference
+        this.game = game;
     }
 
     @Override
     public void show() {
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
-        batch = new SpriteBatch();
 
-        // Load images
         backgroundTexture = new Texture("background.png"); // Same background as other pages
         titleTexture = new Texture("title.png");
         startButtonTexture = new Texture("play-button.png");
         leaderboardButtonTexture = new Texture("leaderboards-button.png");
+        rocketTexture = new Texture("rocket-up.png"); // Load rocket texture
 
-        Table table = new Table();
-        table.setFillParent(true);
-        table.top();
+        Table rootTable = new Table();
+        rootTable.setFillParent(true);
 
         Image backgroundImage = new Image(backgroundTexture);
         backgroundImage.setFillParent(true);
-        stage.addActor(backgroundImage); // Set background
-        stage.addActor(table);
+        stage.addActor(backgroundImage);
 
-        // game title
+        // DO NOT TOUCH VALUES I SPENT 15 MINUTES ADJUUSTING THE VALUES TO MAKE IT PERFECT
         Image titleImage = new Image(titleTexture);
-        titleImage.setSize(990, 350);
-        table.add(titleImage).width(990).height(350).padTop(300).row();
+        rootTable.add(titleImage)
+            .width(900)
+            .height(300)
+            .padBottom(0)
+            .row();
 
-        // space to push buttons down
-        table.add().height(1000).row();
+        // DO NOT TOUCH TOO OR ELSE I WILL KMS LEAVE IT AS IS
+        Image rocketImage = new Image(rocketTexture);
+        rootTable.add(rocketImage)
+            .width(135)
+            .height(100)
+            .padTop(565)
+            .padBottom(320)
+            .row();
 
-        // start button
         Image startButton = new Image(startButtonTexture);
-        startButton.setSize(475, 200);
         startButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 game.setScreen(new GameScreen(game));
             }
         });
-        table.add(startButton).width(475).height(200).padBottom(20).row();
+        rootTable.add(startButton).width(375).height(275).padTop(50).row();
 
-        // leaderboard button
         Image leaderboardButton = new Image(leaderboardButtonTexture);
-        leaderboardButton.setSize(600, 200);
         leaderboardButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new LeaderboardScreen(game)); // Switch to leaderboard
+                game.setScreen(new LeaderboardScreen(game));
             }
         });
-        table.add(leaderboardButton).width(600).height(200).padTop(20);
+        rootTable.add(leaderboardButton).width(600).height(200).padTop(20);
 
-        stage.addActor(table);
+        stage.addActor(rootTable);
     }
 
     @Override
@@ -95,16 +97,17 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void pause() {
-        // Required method, can be left empty
+    //nothing yet queen
     }
 
     @Override
     public void resume() {
-        // Required method, can be left empty
+     //nothing yet
     }
 
     @Override
     public void hide() {
+        // none yet
         stage.dispose();
     }
 
@@ -115,9 +118,6 @@ public class MainMenuScreen implements Screen {
         titleTexture.dispose();
         startButtonTexture.dispose();
         leaderboardButtonTexture.dispose();
+        rocketTexture.dispose();
     }
 }
-
-
-
-
